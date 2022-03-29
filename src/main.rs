f94f2afaf1dce1aec8012982247e01983fe8376b
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-03-29 03:10:29 -0400

    Take file path and optional binary path from CLA

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
 use std::fs::{OpenOptions, self};
 use std::io::prelude::*;
 use std::path::Path;
+use std::env;
 
 use yamasm::lexer::Lexer;
 use yamasm::compiler::Compiler;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <filepath> [<binpath>]", args[0]);
+        return;
+    }
+    let file_path = &args[1];
+    let bin_path = if args.len() > 2 {
+        &args[2]
+    } else {
+        "a.out"
+    };
+
     let mut lexer = Lexer::new();
 
-    let tokens = lexer.tokenize("test.yas");
+    let tokens = lexer.tokenize(&file_path);
     
     let compiler = Compiler::new(tokens);
     
     let instructions = compiler.compile_instructions();
 
-    let file_name = "a.out";
-
-    if Path::new(file_name).exists() {
-        fs::remove_file(file_name).unwrap();
+    if Path::new(bin_path).exists() {
+        fs::remove_file(bin_path).unwrap();
       }
 
     let mut out_file = OpenOptions::new()
         .create_new(true)
         .write(true)
-        .open(file_name)
+        .open(bin_path)
         .unwrap();
 
     match out_file.write_all(&instructions) {
