99fbf994c9f79edc4d172bc5ec418cb4e395eb8c
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-05-21 00:48:29 -0400

    Use sargparse, add debug flags

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,58 @@
+extern crate sargparse;
+
 use std::fs::{OpenOptions, self};
 use std::io::prelude::*;
 use std::path::Path;
-use std::env;
+use sargparse::{ArgumentParser, ArgumentType, InnerData};
 
 use yamasm::lexer::Lexer;
 use yamasm::compiler::Compiler;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: {} <filepath> [<binpath>]", args[0]);
-        return;
-    }
-    let file_path = &args[1];
-    let bin_path = if args.len() > 2 {
-        &args[2]
-    } else {
-        "a.out"
-    };
+    let mut parser = ArgumentParser::new(Some("YamASM - Assembler for YaminiVM"));
+
+    parser.add_argument("f", "file_path", "File path to yas file", 
+                        true, None, ArgumentType::STR);
+    parser.add_argument("-o", "--output", "Output binary file path", 
+                        false, Some(InnerData::STR("a.out".to_string())), ArgumentType::STR);
+    parser.add_argument("-t", "--tokens", "Flag to print tokens",
+                        false, Some(InnerData::BOOL(false)), ArgumentType::BOOL);
+    parser.add_argument("-i", "--instructions", "Flag to print compiled instructions",
+                        false, Some(InnerData::BOOL(false)), ArgumentType::BOOL);
+
+    let args = parser.parse_args().unwrap();
+
+    let file_path = args.get("file_path").unwrap().get_str();
+    let bin_path = &args.get("output").unwrap().get_str();
+
+    let token_flag = args.get("tokens").unwrap().get_bool();
+    let instructions_flag = args.get("instructions").unwrap().get_bool();
 
     let mut lexer = Lexer::new();
 
     let tokens = lexer.tokenize(&file_path);
 
+    if token_flag {
+        println!("--------------------------------------------");
+        println!("Tokens:");
+        for token in &tokens {
+            println!("{:?}", token);
+        }
+        println!("--------------------------------------------");
+    }
+
     let mut compiler = Compiler::new(tokens);
     
     let instructions = compiler.compile_instructions();
+
+    if instructions_flag {
+        println!("--------------------------------------------");
+        println!("Instructions:");
+        for instruction in &instructions {
+            println!("{:?}", instruction);
+        }
+        println!("--------------------------------------------");
+    }
 
     if Path::new(bin_path).exists() {
         fs::remove_file(bin_path).unwrap();
