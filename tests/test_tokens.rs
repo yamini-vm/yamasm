d75597930ebfdf9c825b70a2f3a40d1e47dfbcd9
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-06-20 03:07:20 -0400

    Fix tests

--- tests/test_tokens.rs
+++ tests/test_tokens.rs
@@ -165,7 +165,7 @@ fn test_token_to_bytes() {
     assert_eq!(token.to_bytes(), vec![Some(5)]);
 
     let token = Token::new(TokenType::NUM, "6".to_string());
-    assert_eq!(token.to_bytes(), vec![Some(6)]);
+    assert_eq!(token.to_bytes(), vec![Some(12), Some(54), Some(13)]);
 
     let token = Token::new(TokenType::MOD, "mod".to_string());
     assert_eq!(token.to_bytes(), vec![Some(6)]);
@@ -210,7 +210,7 @@ fn test_token_to_bytes() {
     assert_eq!(token.to_bytes(), vec![Some(18)]);
 
     let token = Token::new(TokenType::NUM, "0".to_string());
-    assert_eq!(token.to_bytes(), vec![Some(0)]);
+    assert_eq!(token.to_bytes(), vec![Some(12), Some(48), Some(13)]);
 
     let token = Token::new(TokenType::ADDR, "1".to_string());
     assert_eq!(token.to_bytes(), vec![Some(1)]);
